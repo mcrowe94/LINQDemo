4b9907b6c0e86dc075cfec93c0dee0b777aa6e66
Monica Crowe <mcrowe9194@gmail.com>
2023-03-27 11:14:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myFamily = new List<string>() { "Monica", "Jake", "Audrey", "Benjamin" };
+
+            var shortList = myFamily.Where(name => name.Length == 4);
+
+            foreach(var family in shortList)
+            {
+                Console.WriteLine(family); //will return family member with only 4 letters in their name
+            }
+        }
+    }
+}
