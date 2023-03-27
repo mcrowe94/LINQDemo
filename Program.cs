c51e577b38cc966dff4c51afdf38e2fd6e514ac0
Monica Crowe <mcrowe9194@gmail.com>
2023-03-27 12:09:35 -0500

    Updated

--- Program.cs
+++ Program.cs
@@ -15,12 +15,29 @@
 
             var numbers = new List<int> { 6, 7, 3, 9, 2, 8, 1, 4, 0, 5 };
 
-            var evens = numbers.Where(numbers => numbers % 2 == 0).OrderBy(x => x);
+            //var evens = numbers.Where(numbers => numbers % 2 == 0).OrderBy(x => x);
 
-            foreach (var item in evens)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in evens)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var lessThan4 = numbers.Where(x => x < 4);
+
+            //foreach(var num in lessThan4)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            var maxNum = numbers.Max();
+            var minNum = numbers.Min();
+            var avgNum = numbers.Average();
+            var sumNum = numbers.Sum();
+            var index = numbers.IndexOf(7);
+
+            Console.WriteLine($"Highest number: {maxNum}");
+            Console.WriteLine($"Lowest number: {minNum}");
+            Console.WriteLine($"Index of 7 value: {index}");
         }
     }
 }
